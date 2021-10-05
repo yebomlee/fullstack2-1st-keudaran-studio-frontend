1635f2f46c0c6260e9fad71f5e1c92e876bdbd9a
siwon <ksw3721@gmail.com>
2021-10-05 16:18:52 +0900

    Add: 좌우 버튼 클릭기능 구현, Mock데이터 추가

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,36 +1,75 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import imgData from './mockData';
 import './Carousel.scss';
 
 class Carousel extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      imgData: imgData,
+      currentImg: 0,
+    };
   }
 
   slideToN = e => {
-    if (e.target.className.charAt(0) === '1') {
+    const dotBtnList = document.getElementsByClassName('dotBtn');
+    if (e.target.className.charAt(3) === '0') {
       document.querySelector('.container').style.transform =
         'translateX(000vw)';
-    } else if (e.target.className.charAt(0) === '2') {
+      document.querySelector('.selected').classList.remove('selected');
+      dotBtnList[0].classList.add('selected');
+    } else if (e.target.className.charAt(3) === '1') {
       document.querySelector('.container').style.transform =
         'translateX(-100vw)';
-    } else if (e.target.className.charAt(0) === '3') {
+      document.querySelector('.selected').classList.remove('selected');
+      dotBtnList[1].classList.add('selected');
+    } else if (e.target.className.charAt(3) === '2') {
       document.querySelector('.container').style.transform =
         'translateX(-200vw)';
+      document.querySelector('.selected').classList.remove('selected');
+      dotBtnList[2].classList.add('selected');
     }
-    // document.querySelector('.btn').classList.toggle('selected');
+  };
+
+  slideRight = e => {
+    if (this.state.currentImg >= this.state.imgData.length - 1) return;
+    let ImgIdx = this.state.currentImg + 1;
+    document.querySelector('.container').style.transform = `translateX(${-(
+      ImgIdx +
+      '' +
+      '00'
+    )}vw)`;
+    this.setState({ currentImg: ImgIdx }, () => {
+      document.querySelector('.selected').classList.remove('selected');
+      document
+        .querySelector(`.img${this.state.currentImg}`)
+        .classList.toggle('selected');
+    });
+  };
+
+  slideLeft = e => {
+    if (this.state.currentImg <= 0) return;
+    let ImgIdx = this.state.currentImg - 1;
+    document.querySelector('.container').style.transform = `translateX(${-(
+      ImgIdx +
+      '' +
+      '00'
+    )}vw)`;
+    this.setState({ currentImg: ImgIdx }, () => {
+      document.querySelector('.selected').classList.remove('selected');
+      document
+        .querySelector(`.img${this.state.currentImg}`)
+        .classList.toggle('selected');
+    });
   };
 
   render() {
     return (
       <div className="Carousel">
         <div className="container">
-          {/* <div className="inner">
-            <img
-              src="https://images.unsplash.com/photo-1624565698535-b0be89187ff4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1986&q=80"
-              alt="Carousel1"
-            />
-          </div> */}
           <div className="inner">
             <img
               src="https://images.unsplash.com/photo-1630694040916-317e3f7d28f6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2030&q=80"
@@ -49,18 +88,22 @@ class Carousel extends Component {
               alt="Carousel1"
             />
           </div>
-          {/* <div className="inner">
-            <img
-              src="https://images.unsplash.com/photo-1624565698535-b0be89187ff4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1986&q=80"
-              alt="Carousel1"
-            />
-          </div> */}
         </div>
-        <div className="btnWraper">
-          <button className="1th btn selected" onClick={this.slideToN} />
-          <button className="2nd btn" onClick={this.slideToN} />
-          <button className="3rd btn" onClick={this.slideToN} />
+        <div className="dotBtnWraper">
+          <button className="img0 dotBtn selected" onClick={this.slideToN} />
+          <button className="img1 dotBtn" onClick={this.slideToN} />
+          <button className="img2 dotBtn" onClick={this.slideToN} />
         </div>
+        <FontAwesomeIcon
+          icon={faAngleLeft}
+          className="leftBtn"
+          onClick={this.slideLeft}
+        />
+        <FontAwesomeIcon
+          icon={faAngleRight}
+          className="rightBtn"
+          onClick={this.slideRight}
+        />
       </div>
     );
   }
---
+++ src/components/Carousel/mockData.js
@@ -0,0 +1,24 @@
+const carouselImgs = [
+  {
+    id: 0,
+    imgUrl:
+      'https://images.unsplash.com/photo-1630694040916-317e3f7d28f6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2030&q=80',
+  },
+  {
+    id: 1,
+    imgUrl:
+      'https://images.unsplash.com/photo-1630693912525-7a833b62c81f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2030&q=80',
+  },
+  {
+    id: 2,
+    imgUrl:
+      'https://images.unsplash.com/photo-1630839437035-dac17da580d0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2030&q=80',
+  },
+  // {
+  //   id: 3,
+  //   imgUrl:
+  //     'https://images.unsplash.com/photo-1624565698535-b0be89187ff4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1986&q=80',
+  // },
+];
+
+export default carouselImgs;
