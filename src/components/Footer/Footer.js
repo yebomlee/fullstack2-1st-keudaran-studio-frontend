fbc84486f9d0a038adab7edeb0aa4a96b8a76f2e
ebong <eebzaaa@gmail.com>
2021-10-08 04:51:34 +0900

    Add: Footer 구현 완료

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import './Footer.scss';
+import {
+  companyInfo,
+  companyInfo2,
+  companyInfo3,
+  companyInfo4,
+} from './FooterData';
 
 class Footer extends Component {
   render() {
@@ -23,43 +29,44 @@ class Footer extends Component {
             </ul>
 
             <ul className="companyInfo">
-              <li className="info">
-                상호 : 주식회사 크다란<span className="verticalBar">|</span>
-              </li>
-              <li className="info">
-                대표 : 조구만<span className="verticalBar">|</span>
-              </li>
-              <li className="info">개인정보보호책임자 : 조구만</li>
+              {companyInfo.map(data => {
+                return (
+                  <li className="info">
+                    {data.content}
+                    <span className="verticalBar">|</span>
+                  </li>
+                );
+              })}
             </ul>
             <ul className="companyInfo">
-              <li className="info">
-                TEL : 00-0000-0000<span className="verticalBar">|</span>
-              </li>
-              <li className="info">
-                EMAIL : kedaran@kedaran.com
-                <span className="verticalBar">|</span>
-              </li>
-              <li className="info">
-                입금 계좌 : [크다란] 000-0000-0000 주식회사 크다란
-              </li>
+              {companyInfo2.map(data => {
+                return (
+                  <li className="info">
+                    {data.content}
+                    <span className="verticalBar">|</span>
+                  </li>
+                );
+              })}
             </ul>
             <ul className="companyInfo">
-              <li className="info">
-                사업자등록번호 : 000-00-00000
-                <span className="verticalBar">|</span>
-              </li>
-              <li className="info">
-                통신판매업신고 : 2021-대한민국서울-0000
-                <span className="verticalBar">|</span>
-              </li>
-              <li className="info">[사업자정보확인]</li>
+              {companyInfo3.map(data => {
+                return (
+                  <li className="info">
+                    {data.content}
+                    <span className="verticalBar">|</span>
+                  </li>
+                );
+              })}
             </ul>
             <ul className="companyInfo">
-              <li className="info">
-                주소 : 서울특별시 강남구 삼성동 테헤란로 427
-                <span className="verticalBar">|</span>
-              </li>
-              <li className="info">www.kedaranstore.com</li>
+              {companyInfo4.map(data => {
+                return (
+                  <li className="info">
+                    {data.content}
+                    <span className="verticalBar">|</span>
+                  </li>
+                );
+              })}
             </ul>
             <p className="copyright">
               copyrightⓒ 2021 주식회사 크다란 ALL RIGHTS RESERVED.
--- src/pages/Main/Main.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Main extends React.Component {
-  render() {
-    return <div></div>;
-  }
-}
-
-export default Main;
