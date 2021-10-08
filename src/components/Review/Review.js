2b706d6ffdadc53ceafaaee61eb2684a0d2b832f
wook95 <uclee95@gmail.com>
2021-10-09 04:15:50 +0900

    Add: 리뷰 글 발행 검증 로직 추가

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -5,29 +5,64 @@ class Review extends React.Component {
     super();
     this.state = {
       review: '',
+      starRating: 0,
       reviewList: [],
+      isPolicyDetail: false,
+      agreePolicy: { agreePolicyRadio: true, notAgreePolicyRadio: false },
     };
   }
 
-  handleClick = e => {
+  createNewReview = e => {
     e.preventDefault();
+    const { agreePolicy, starRating, review } = this.state;
+    if (agreePolicy.notAgreePolicyRadio)
+      return alert('약관에 동의가 필요합니다.');
+    else if (starRating === 0) return alert('별점을 선택해주세요');
+    else if (review === '') return alert('리뷰 내용을 입력해주세요.');
     this.setState({
-      reviewList: [...this.state.reviewList, { content: this.state.review }],
+      reviewList: [
+        ...this.state.reviewList,
+        { content: this.state.review, rating: +this.state.starRating },
+      ],
       review: '',
     });
   };
 
-  handleReview = event => {
+  handelInputChange = event => {
+    const { name, value } = event.target;
     this.setState({
-      review: event.target.value,
+      [name]: value,
+    });
+  };
+
+  decideStarRating = num => {
+    let star = '';
+    for (let i = 0; i < num; i++) {
+      star += '★';
+    }
+    return star;
+  };
+
+  showPolicyDetail = () => {
+    const { isPolicyDetail } = this.state;
+    this.setState({ isPolicyDetail: !isPolicyDetail });
+  };
+
+  checkPolicyRadio = e => {
+    const { agreePolicy } = this.state;
+    this.setState({
+      agreePolicy: {
+        agreePolicyRadio: !agreePolicy.agreePolicyRadio,
+        notAgreePolicyRadio: !agreePolicy.notAgreePolicyRadio,
+      },
     });
   };
 
   render() {
-    const { reviewList } = this.state;
+    const { reviewList, isPolicyDetail, agreePolicy } = this.state;
     return (
       <div className="Review">
-        {console.log(this.state.reviewList)}
+        {console.log(this.state.agreePolicy)}
         <div className="reviewInner">
           <h1 className="reviewTitle">REVIEW</h1>
           <div className="reviewRate">
@@ -55,7 +90,11 @@ class Review extends React.Component {
           <div className="reviewWrite">
             <form className="reviewForm">
               <div className="howManyStars">
-                <select name="rating" className="starSelector">
+                <select
+                  name="starRating"
+                  className="starSelector"
+                  onChange={this.handelInputChange}
+                >
                   <option value="" selected>
                     -- 별점 선택 --
                   </option>
@@ -68,74 +107,91 @@ class Review extends React.Component {
               </div>
               <textarea
                 className="reviewInput"
-                name="content"
+                value={this.state.review}
+                name="review"
                 placeholder="
                 크다란을 이용해 주셔서 감사합니다. 리뷰는 품질개선과
                 서비스향상에 도움이 됩니다."
-                onChange={this.handleReview}
+                onChange={this.handelInputChange}
               ></textarea>
-              <button className="reviewSubmitButton" onClick={this.handleClick}>
+              <button
+                className="reviewSubmitButton"
+                onClick={this.createNewReview}
+              >
                 {/* 눌렀을때 로그인여부확인 안되어있으면 alert -> 백엔드 연결시 추가해야함*/}
                 리뷰 등록
               </button>
             </form>
             <div className="privacyPolicy">
               <div className="privacyTitle">
-                개인정보수집이용
-                <input
-                  type="radio"
-                  name="agree"
-                  value="1"
-                  checked
-                  id="agreeReview"
-                />
-                <label className="agreeOrNot" htmlFor="agreeReview">
-                  <b>동의</b>{' '}
-                </label>
-                <input
-                  type="radio"
-                  name="agree"
-                  value="0"
-                  id="notAgreeReview"
-                />
-                <label className="agreeOrNot" htmlFor="notAgreeReview">
-                  <b>미동의</b>
-                </label>
+                <p className="privacyTitleInner">
+                  <b>개인정보수집이용</b>
+                  <u onClick={this.showPolicyDetail}>자세히 보기</u>
+                </p>
+
+                <div class="privacyRadioBox">
+                  <input
+                    type="radio"
+                    name="agree"
+                    value="1"
+                    checked={agreePolicy.agreePolicyRadio}
+                    id="agreePolicyRadio"
+                    onChange={this.checkPolicyRadio}
+                  />
+                  <label className="agreeOrNot" htmlFor="agreePolicyRadio">
+                    <b>동의</b>
+                  </label>
+                  <input
+                    type="radio"
+                    name="agree"
+                    value="0"
+                    checked={agreePolicy.notAgreePolicyRadio}
+                    id="notAgreePolicyRadio"
+                    onChange={this.checkPolicyRadio}
+                  />
+                  <label className="agreeOrNot" htmlFor="notAgreePolicyRadio">
+                    <b>미동의</b>
+                  </label>
+                </div>
               </div>
-              <table className="privacyTable">
-                <colgroup>
-                  <col width="200"></col>
-                  <col width="250"></col>
-                  <col width="130"></col>
-                </colgroup>
-                <thead className="tableHead">
-                  <tr>
-                    <th scope="col">목적</th>
-                    <th scope="col">항목</th>
-                    <th scope="col">보유기간</th>
-                  </tr>
-                </thead>
-                <tbody className="tableBody">
-                  <tr>
-                    <td className="perpose">
-                      <div className="privacyTxt">
-                        게시판 서비스 제공 / 이용 고객 확인
-                      </div>
-                    </td>
-                    <td className="items">
-                      <div className="privacyTxt">
-                        이름, 비밀번호, 작성내용, IP주소
-                      </div>
-                    </td>
-                    <td className="hold">
-                      <div className="privacyTxt">게시글 삭제시까지</div>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-              <div className="agreeForFillOut">
-                * 동의하셔야 서비스를 이용하실 수 있습니다.
-              </div>
+              {isPolicyDetail ? (
+                <>
+                  <table className="privacyTable">
+                    <colgroup>
+                      <col width="200"></col>
+                      <col width="250"></col>
+                      <col width="130"></col>
+                    </colgroup>
+                    <thead className="tableHead">
+                      <tr>
+                        <th scope="col">목적</th>
+                        <th scope="col">항목</th>
+                        <th scope="col">보유기간</th>
+                      </tr>
+                    </thead>
+                    <tbody className="tableBody">
+                      <tr>
+                        <td className="perpose">
+                          <div className="privacyTxt">
+                            게시판 서비스 제공 / 이용 고객 확인
+                          </div>
+                        </td>
+                        <td className="items">
+                          <div className="privacyTxt">
+                            이름, 비밀번호, 작성내용, IP주소
+                          </div>
+                        </td>
+                        <td className="hold">
+                          <div className="privacyTxt">게시글 삭제시까지</div>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <div className="agreeForFillOut">
+                    * 동의하셔야 서비스를 이용하실 수 있습니다.
+                  </div>
+                </>
+              ) : null}
             </div>
           </div>
 
@@ -148,14 +204,16 @@ class Review extends React.Component {
                     <li className="createdReview">
                       <div className="starsAndData">
                         <span className="stars">
-                          {review.rating}⭐️⭐️⭐️⭐️⭐️
+                          {this.decideStarRating(review.rating)}
                         </span>
-                        <li className="userId">
-                          서버에서 받아올 아이디 {review.userId}
-                        </li>
-                        <li className="createdAt">
-                          서버에서 받아올 시간 {review.createdAt}
-                        </li>
+                        <div className="reviewUserInfo">
+                          <span className="userId">
+                            서버에서 받아올 아이디 {review.userId}
+                          </span>
+                          <span className="createdAt">
+                            서버에서 받아올 시간 {review.createdAt}
+                          </span>
+                        </div>
                       </div>
                       <div className="reviewContent">
                         <textarea
