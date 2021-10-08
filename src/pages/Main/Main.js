4029a96470714a60fa57bdc5ea8629c9102a0b05
ebong <eebzaaa@gmail.com>
2021-10-08 15:14:33 +0900

    fix: 리뷰 반영 완료

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import Footer from '../../components/Footer/Footer';
 
 class Main extends React.Component {
   render() {
-    return (
-      <div>
-        <Footer />
-      </div>
-    );
+    return <div></div>;
   }
 }
 
